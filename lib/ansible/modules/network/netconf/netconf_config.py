09326c1493674423d0b05ca2b907a4559d608fa6
NICOLAS Alain <a.nicolas@monaco-telecom.mc>
2018-05-16 14:25:49 +0200

    Test

--- lib/ansible/modules/network/netconf/netconf_config.py
+++ lib/ansible/modules/network/netconf/netconf_config.py
@@ -2,6 +2,7 @@
 
 # (c) 2016, Leandro Lisboa Penz <lpenz at lpenz.org>
 # GNU General Public License v3.0+ (see COPYING or https://www.gnu.org/licenses/gpl-3.0.txt)
+# Test
 
 from __future__ import absolute_import, division, print_function
 __metaclass__ = type
